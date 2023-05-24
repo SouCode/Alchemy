ef4a7ce8accc3113ae410f755082d4066ed7ac51
Ronsou <93101540+SouCode@users.noreply.github.com>
2023-05-24 03:51:56 +0000

    Setting up Google Oath

---
+++ Backend/Utils/GoogleAuth.js
@@ -0,0 +1,26 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+require('dotenv').config();
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      // Handle the user profile details here
+      done(null, profile);
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
-// server.js
-
 const express = require('express');
 const connectDB = require('./Utils/Database');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
 
 // Load environment variables
 require('dotenv').config();
@@ -12,10 +12,27 @@ const app = express();
 // Connect to MongoDB
 connectDB();
 
+// Google OAuth Setup
+require('./Utils/GoogleAuth');
+
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Middleware for handling cookies
+app.use(
+  cookieSession({
+    maxAge: 24 * 60 * 60 * 1000, // One day in milliseconds
+    keys: [process.env.COOKIE_KEY]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Routes setup
+const AuthRoutes = require('./Routes/AuthRoutes');  // Require Google OAuth routes
+app.use('/', AuthRoutes);
 
 // Start the server
 const port = process.env.PORT || 5000;
